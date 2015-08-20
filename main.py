67fe40edabd4528832011b978cd447c46a45a947
Gustavohqo <gustavo.hqo@gmail.com>
2015-08-20 08:47:42 -0300

    Modificaçoes em DessertView

--- main.py
+++ main.py
@@ -11,11 +11,18 @@ import json
 
 import webapp2
 
+
 class Ingredient(ndb.Model):
     name = ndb.StringProperty(indexed=False)
     total_amount = ndb.FloatProperty(indexed=False)
     metric = ndb.StringProperty(indexed=False)
     total_cost = ndb.FloatProperty(indexed=False)
+
+
+class UsedIngredient(ndb.Model):
+    ingredient = ndb.StructuredProperty(Ingredient, indexed=False)
+    total_amount = ndb.FloatProperty(indexed=False)
+
 
 class Request(ndb.Model):
     name = ndb.StringProperty(indexed=False)
@@ -23,13 +30,15 @@ class Request(ndb.Model):
     metric = ndb.StringProperty(indexed=False)
     total_cost = ndb.FloatProperty(indexed=False)
 
+
 class Dessert(ndb.Model):
     """Model Sobremesa"""
     name = ndb.StringProperty(indexed=False)
-    ingredient = ndb.StructuredProperty(Ingredient, repeated=True)
-    numb_of_portions = ndb.IntegerProperty(indexed=False)
+    ingredient_list = ndb.StructuredProperty(UsedIngredient, repeated=True)
+    portion_amount = ndb.IntegerProperty(indexed=False)
     portion_cost = ndb.FloatProperty(indexed=False)
     cooker_name = ndb.StringProperty(indexed=False)
+
 
 class Client(ndb.Model):
     """Model Cliente"""
@@ -40,20 +49,35 @@ class Client(ndb.Model):
 
 class DessertService(webapp2.RequestHandler):
     def get(self):
-        dessertList = json.dumps([dessert.to_dict() for dessert in Dessert.query().fetch()])
-        self.response.write(dessertList)
+        dessert_list = json.dumps([dessert.to_dict() for dessert in Dessert.query().fetch()])
+        self.response.write(dessert_list)
+
+    def get_ingredient_list(self, data):
+
+        ing = []
+        for ingredient in data:
+            ing.append(ingredient)
+        return ing
 
     def post(self):
-        logging.debug("LOG: POST-")
+        logging.debug("LOG: POST- On DessertService")
         dessert = json.loads(self.request.body)
+        #
+        # newDessert = Dessert()
+        # newDessert.name = dessert["name"]
+        # newDessert.ingredient_list = self.___get_ingredient_list(dessert["ingredient_list"])
+        # newDessert.cooker_name = dessert["cooker_name"]
+        # newDessert.numb_of_portions = dessert["portion_amount"]
+        # newDessert.portion_cost = dessert["portion_cost"]
+        #
+        # newDessert.put()
+        logging.debug(dessert["ingredient_list"])
 
-        newDessert = Dessert()
-        newDessert.name = dessert["name"]
+    # ___get_ingredient_list = get_ingredient_list
 
-        self.response.write(newDessert.name)
 
 class MainClient(webapp2.RequestHandler):
-    def post(self):
+     def post(self):
         logging.debug("LOG: POST-")
         client = json.loads(self.request.body)
 
@@ -63,10 +87,12 @@ class MainClient(webapp2.RequestHandler):
 
         self.response.write(newClient.name)
 
+
 class IngredientService(webapp2.RequestHandler):
     def get(self):
         ingredientList = json.dumps([ing.to_dict() for ing in Ingredient.query().fetch()])
         self.response.write(ingredientList)
+
     def post(self):
         newIngredient = json.loads(self.request.body)
         ingredient = Ingredient()
@@ -81,6 +107,7 @@ class RequestService(webapp2.RequestHandler):
     def get(self):
         requestList = json.dumps([ing.to_dict() for ing in Request.query().fetch()])
         self.response.write(requestList)
+
     def post(self):
         newRequest = json.loads(self.request.body)
         request = Request()
